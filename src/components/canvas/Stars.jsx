81c7d2bc89c74320aa6dc3e82362777217e7b0b7
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-04-08 09:58:02 +0530

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -18,7 +18,7 @@ const Stars = (props) => {
         <PointMaterial
           transparent
           color='#f272c8'
-          size={0.001}
+          size={0.0010}
           sizeAttenuation={true}
           depthWrite={false}
         />
