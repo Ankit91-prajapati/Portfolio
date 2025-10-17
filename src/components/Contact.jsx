343c854a1d8f437e82f29b05da7b98ed3533b265
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-10-17 19:04:22 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .sendForm(
+      .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         e.target,
