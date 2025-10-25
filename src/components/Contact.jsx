b6b611048850ac9c6542ddcfd913c76f2b710ca3
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-10-25 11:05:25 +0530

    Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,27 +28,22 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        e.target,
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
-
-        {
-          subject: form.subject,
-          from_name: form.name,
-          to_name: "Ankit", // (optional if not used in template)
-          from_email: form.email,
-          to_email: "ankitprajapati962173@gmail.com", // (optional if not used in template)
-          reply_to: form.email,
-
-          // 8bc2c0ec70a431c08fee48beeada18c1afbf4555
-          message: form.message,
-          time: new Date().toLocaleString(),
-        },
-
-       
-      )
+    emailjs
+  .send(
+    import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    {
+      subject: form.subject,
+      from_name: form.name,
+      to_name: "Ankit",
+      from_email: form.email,
+      to_email: "ankitprajapati962173@gmail.com",
+      reply_to: form.email,
+      message: form.message,
+      time: new Date().toLocaleString(),
+    },
+    import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+  )
       .then(
         () => {
           setLoading(false);
