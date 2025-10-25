145305829353edb5c2a899be95df9ded54e64b20
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-10-25 17:53:01 +0530

    Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,8 +27,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-  .send(
+    emailjs.send(
     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
     {
@@ -36,7 +35,6 @@ const Contact = () => {
       from_name: form.name,
       to_name: "Ankit",
       from_email: form.email,
-      to_email: "ankitprajapati962173@gmail.com",
       reply_to: form.email,
       message: form.message,
       time: new Date().toLocaleString(),
